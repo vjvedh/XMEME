d1fec6efbad86efd3cc32e582cb8583b59673fcb
vjvedh <vishallala99@gmail.com>
2021-02-13 00:46:16 +0530

    Set up Home UI #1

---
+++ xmeme_frontend/lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+import 'models/global.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'XMEME',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Colors.white,
+      home: SafeArea(
+        child: DefaultTabController(
+          length: 2,
+          child: new Scaffold(
+            body: Stack(children: <Widget>[
+              TabBarView(
+                children: [
+                  new Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomLeft,
+                        end: Alignment.topRight,
+                        colors: [
+                          blueColor,
+                          purpleColor,
+                          redColor,
+                        ],
+                      ),
+                    ),
+                  ),
+                  new Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          redColor,
+                          purpleColor,
+                          blueColor,
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: 160,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+                child: Center(
+                  child: Text("XMeme", style: navBarText),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                margin: EdgeInsets.only(
+                    top: 132, left: MediaQuery.of(context).size.width * 0.5),
+                child: FloatingActionButton(
+                  child: Icon(
+                    Icons.add,
+                    size: 30,
+                  ),
+                  backgroundColor: redColor,
+                  onPressed: () {},
+                ),
+              ),
+            ]),
+            appBar: AppBar(
+              elevation: 0,
+              title: new TabBar(
+                tabs: [
+                  Tab(icon: new Icon(Icons.art_track_rounded)),
+                  Tab(
+                    icon: new Icon(Icons.add_a_photo_rounded),
+                  ),
+                ],
+                labelColor: darkGray,
+                unselectedLabelColor: Colors.blue,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorPadding: EdgeInsets.all(5.0),
+                indicatorColor: Colors.transparent,
+              ),
+              backgroundColor: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ xmeme_frontend/lib/models/global.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color darkGray = new Color.fromARGB(255, 32, 32, 32);
+Color redColor = new Color.fromARGB(255, 250, 0, 0);
+Color blueColor = new Color.fromARGB(255, 0, 0, 170);
+Color purpleColor = new Color.fromARGB(255, 130, 0, 80);
+
+TextStyle navBarText = GoogleFonts.lato(
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+  fontSize: 40,
+);
