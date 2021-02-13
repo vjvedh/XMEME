a82d7426a5ef5e380472bb815bee172ea4ccfa02
vjvedh <vishallala99@gmail.com>
2021-02-14 02:04:05 +0530

    To Add Cards #4

--- xmeme_frontend/lib/main.dart
+++ xmeme_frontend/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:xmeme_frontend/screens/IntrayPage.dart';
-import 'models/global.dart';
+import 'package:xmeme_frontend/screens/HomeScreen/Homescreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,101 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'XMEME',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      color: Colors.white,
-      home: SafeArea(
-        child: DefaultTabController(
-          length: 2,
-          child: new Scaffold(
-            body: Stack(children: <Widget>[
-              TabBarView(
-                children: [
-                  new IntrayPage(),
-                  new Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          redColor,
-                          purpleColor,
-                          blueColor,
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Container(
-                height: 160,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50),
-                  ),
-                ),
-                child: Center(
-                  child: Text("XMeme", style: navBarText),
-                ),
-              ),
-              Container(
-                height: 50,
-                width: 50,
-                margin: EdgeInsets.only(
-                    top: 132, left: MediaQuery.of(context).size.width * 0.5),
-                child: FloatingActionButton(
-                  child: Icon(
-                    Icons.add,
-                    size: 30,
-                  ),
-                  backgroundColor: redColor,
-                  onPressed: () {},
-                ),
-              ),
-            ]),
-            appBar: AppBar(
-              elevation: 0,
-              title: new TabBar(
-                tabs: [
-                  Tab(icon: new Icon(Icons.art_track_rounded)),
-                  Tab(
-                    icon: new Icon(Icons.add_a_photo_rounded),
-                  ),
-                ],
-                labelColor: darkGray,
-                unselectedLabelColor: Colors.blue,
-                indicatorSize: TabBarIndicatorSize.label,
-                indicatorPadding: EdgeInsets.all(5.0),
-                indicatorColor: Colors.transparent,
-              ),
-              backgroundColor: Colors.white,
-            ),
-          ),
-        ),
-      ),
+      home: MyHomePage(title: 'XMEME'),
     );
   }
 }
--- xmeme_frontend/lib/models/global.dart
+++ xmeme_frontend/lib/models/global.dart
@@ -1,13 +1,55 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 Color darkGray = new Color.fromARGB(255, 32, 32, 32);
 Color redColor = new Color.fromARGB(255, 250, 0, 0);
 Color blueColor = new Color.fromARGB(255, 0, 0, 170);
 Color purpleColor = new Color.fromARGB(255, 130, 0, 80);
 
-TextStyle navBarText = GoogleFonts.lato(
-  fontWeight: FontWeight.bold,
-  color: Colors.black,
-  fontSize: 40,
-);
+const String HomeRoute = '/memes';
+const String PostRoute = '/about';
+
+class GlassCard extends StatelessWidget {
+  final List<Widget> childItems;
+
+  const GlassCard({Key key, this.childItems}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+              blurRadius: 26,
+              spreadRadius: 16,
+              color: Colors.black.withOpacity(0.2))
+        ]),
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 20.0,
+              sigmaY: 20.0,
+            ),
+            child: Container(
+              margin: EdgeInsets.all(5),
+              height: size.height * 0.4,
+              width: size.height * 0.5,
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.2),
+                  borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                  border: Border.all(
+                      width: 1.5, color: Colors.white.withOpacity(0.2))),
+              child: Column(
+                children: childItems,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
